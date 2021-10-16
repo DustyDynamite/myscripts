ec553fba18b902d943733ed14e852691bf0a0f67
mobeen <rage075@gmail.com>
2021-10-16 16:44:11 +0500

    added some scripts

---
+++ hidefloat.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+#i3-msg "focus mode_toggle"
+
+i3-msg "move position 1365 px 767 px"
+i3-msg "focus left"
+i3-msg "move position 1365 px 767 px"
+i3-msg "focus left"
+i3-msg "move position 1365 px 767 px"
+i3-msg "focus left"
+i3-msg "move position 1365 px 767 px"
+i3-msg "focus left"
+i3-msg "move position 1365 px 767 px"
+i3-msg "focus left"
+
+i3-msg "focus mode_toggle"
---
+++ showfloat.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+i3-msg "focus mode_toggle"
+
+i3-msg "move absolute position center"
+i3-msg "focus left"
+i3-msg "move absolute position center"
+i3-msg "focus left"
+i3-msg "move absolute position center"
+i3-msg "focus left"
+i3-msg "move absolute position center"
+i3-msg "focus left"
+i3-msg "move absolute position center"
+i3-msg "focus left"
+
+#i3-msg "focus mode_toggle"
