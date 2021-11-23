2253ed5cb9f6c3d3a12cd03739e1b5d87e4b64dd
mobeen <rage075@gmail.com>
2021-11-23 19:18:27 +0500

    hsfloat.py and machine learning coding efforts

--- hidefloat.sh
+++ hidefloat.sh
@@ -1,17 +1,9 @@
 #! /bin/bash
 
 
+
+i3-msg "[workspace=__focused__ floating] move position 1365 px 767 px"
+
+i3-msg "[workspace=__focused__ tiling] focus"
+
 #i3-msg "focus mode_toggle"
-
-i3-msg "move position 1365 px 767 px"
-i3-msg "focus left"
-i3-msg "move position 1365 px 767 px"
-i3-msg "focus left"
-i3-msg "move position 1365 px 767 px"
-i3-msg "focus left"
-i3-msg "move position 1365 px 767 px"
-i3-msg "focus left"
-i3-msg "move position 1365 px 767 px"
-i3-msg "focus left"
-
-i3-msg "focus mode_toggle"
--- showfloat.sh
+++ showfloat.sh
@@ -1,17 +1,9 @@
 #! /bin/bash
 
 
-i3-msg "focus mode_toggle"
-
-i3-msg "move absolute position center"
-i3-msg "focus left"
-i3-msg "move absolute position center"
-i3-msg "focus left"
-i3-msg "move absolute position center"
-i3-msg "focus left"
-i3-msg "move absolute position center"
-i3-msg "focus left"
-i3-msg "move absolute position center"
-i3-msg "focus left"
-
 #i3-msg "focus mode_toggle"
+
+i3-msg "[workspace=__focused__ floating] move absolute position center"
+
+i3-msg "[workspace=__focused__ floating] focus"
+
